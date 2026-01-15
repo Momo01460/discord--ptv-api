c3747e9c37d4eecee0033ad3c80ba38a62716fe8
Momo01460 <mohamed.aouraini01460@gmail.com>
2026-01-16 00:07:59 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+import json
+
+TOKEN = "MTQ2MTQ5MjY0ODM1NDUxNzEzNw.Gr60xr.Iy_FS3BG0u40hHpe64Lpm-r2deftOtTjzt21z4"
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.dm_messages = True
+
+client = discord.Client(intents=intents)
+
+DATA_FILE = "data.json"
+
+def load_data():
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
+
+@client.event
+async def on_ready():
+    print(f"Bot connecté en tant que {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == "!last":
+        data = load_data()
+        if data["keys"]:
+            last = data["keys"][-1]
+            await message.author.send(
+                f"✅ Paiement confirmé\n"
+                f"🔐 Ton accès : {last['key']}\n"
+                f"📅 Expire le : {last['expire']}"
+            )
+        else:
+            await message.author.send("❌ Aucune clé trouvée.")
+
+client.run(TOKEN)
